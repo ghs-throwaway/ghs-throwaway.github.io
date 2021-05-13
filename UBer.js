15a0be0695df07c825923cf4d7f1a82d6e14f11d
ghs-throwaway <81245916+ghs-throwaway@users.noreply.github.com>
2021-05-13 11:52:05 -0500

    Update UBer.js

--- UBer.js
+++ UBer.js
@@ -37,7 +37,10 @@ enter url. make sure to start with http or:// https://.\n\n\n\
   if ("" != url && null != url && "https://a.mutualbooks.biz/go/https://null" != url) {
     alert("DEBUG no cancel detected");
     if (url.includes("//", 0) || url.includes("http://", 0) || url.includes("https://", 0)) {
-      console.log("why tf is this not working?")
+      console.log("why tf is this not working?");
+      console.log("like seriously");
+      document.body.innerHTML = "test";
+      console.log("pls?")
       window.onbeforeunload = function() {
         /* lyft implementation built-in Just in Case(tm) :) */
         console.log("Lyft implementation courtesy of UBer");
