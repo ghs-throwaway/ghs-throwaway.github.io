bb2fa047d43377c6098a95c8a5b71cfff9eaedd7
ghs-throwaway <81245916+ghs-throwaway@users.noreply.github.com>
2021-05-12 23:09:54 -0500

    use semicolons :face_vomiting:

--- UBer.js
+++ UBer.js
@@ -9,26 +9,24 @@
 
 // TODO fucking fix this you braindead child
 
-function UBer() {
+void function() {
   (function() {
     var url = prompt("\
 Welcome to UBer v3.1\n\
 enter url, the one already there is Google. make sure to start with http or:// https://.\n\
 OR enter 'proxy' to go to a proxy site.\n\n\n\
 -luphoria",
-    placeholder="//www.google.com/webhp?igu=1")
+    placeholder="//www.google.com/webhp?igu=1");
     if(url == "proxy") {
       var url2 = prompt("\
 Welcome to UBer v3.1\n\
 enter url. make sure to start with http or:// https://.\n\n\n\
 -luphoria",
-      placeholder="https://google.com/")
+      placeholder="https://google.com/");
       if(url2 != "" && null != url2) { url = "https://a.mutualbooks.biz/go/" + url2 } /* HU proxy site by QuiteAFancyEmerald -- TODO create my own */ 
-    }
-    "" != url && null != url && "proxy" != url && "https://a.mutualbooks.biz/go/https://null" != url && document.write("<title>UBer 3.1</title><div position:absolute; left: 0; right: 0; bottom: 0; top: 0px; ><iframe src='"+url+"' id='uberFrame' style=' position: fixed; top: 0px; bottom: 0px; right: 0px; width: 100%; border: none; margin: 0; padding: 0; overflow: hidden; z-index: 999999; height: 100%;' >></iframe></div>")})()}()
+    };
+    "" != url && null != url && "proxy" != url && "https://a.mutualbooks.biz/go/https://null" != url && document.write("<title>UBer 3.1</title><div position:absolute; left: 0; right: 0; bottom: 0; top: 0px; ><iframe src='"+url+"' id='uberFrame' style=' position: fixed; top: 0px; bottom: 0px; right: 0px; width: 100%; border: none; margin: 0; padding: 0; overflow: hidden; z-index: 999999; height: 100%;' >></iframe></div>")})()}();
 window.onbeforeunload = function() { 
-  console.log("Lyft implementation courtesy of UBer") 
-  return "UBer on top." 
+  console.log("Lyft implementation courtesy of UBer");
+  return "UBer on top.";
 }
-
-UBer()
