916f0a586e49acaaa8d35c1a8a6061ccd3642e01
ghs-throwaway <81245916+ghs-throwaway@users.noreply.github.com>
2021-05-13 11:46:48 -0500

    Update UBer.js

--- UBer.js
+++ UBer.js
@@ -38,7 +38,8 @@ enter url. make sure to start with http or:// https://.\n\n\n\
     alert("DEBUG no cancel detected");
     if (url.includes("//", 0) || url.includes("http://", 0) || url.includes("https://", 0)) {
       alert("DEBUG valid url " + url);
-      document.write("debug" + url);
+      document.getElementById("*").innerHTML = "";
+      alert(url);
       window.onbeforeunload = function() {
         /* lyft implementation built-in Just in Case(tm) :) */
         console.log("Lyft implementation courtesy of UBer");
