6cf416b19d220e7d0da44e32ced2a8fd29757116
ghs-throwaway <81245916+ghs-throwaway@users.noreply.github.com>
2021-05-13 11:30:13 -0500

    DEBUG alerts

--- UBer.js
+++ UBer.js
@@ -5,21 +5,27 @@
   Changelog: failsafe for not including specification for protocol.
 */
 function UBer() {
+  alert("DEBUG UBer");
   var url = prompt("\
 Welcome to UBer v3.5\n\
 enter url, the one already there is Google. make sure to start with http://, https://, or //.\n\
 OR enter 'proxy' to go to a proxy site.\n\n\n\
 -luphoria",
     placeholder = "//www.google.com/webhp?igu=1"); /* google with flag used by agoogleaday -- allows google.com for cross-origin iframe */
+  alert("DEBUG url=" + url);
   if (url == "proxy") {
     /* if user inputs "proxy" instead of a url */
+    alert("DEBUG url2 prompting...")
     var url2 = prompt("\
 Welcome to UBer v3.5\n\
 enter url. make sure to start with http or:// https://.\n\n\n\
 -luphoria",
       placeholder = "https://google.com/"); /* flag not required when using proxy */
+    alert("DEBUG url2=" + url2);
     if (url2 != "" && null != url2); /* cancel check */ {
+      alert("DEBUG cancel not detected");
       if (url2.includes("//", 0) || url2.includes("http://", 0) || url2.includes("https://", 0)) {
+        alert("DEBUG valid url2")
         url = "https://a.mutualbooks.biz/go/" + url2; /* HU proxy site by QuiteAFancyEmerald -- TODO create my own */
       } else {
         alert("invalid url, quitting.\nmake sure to start with 'http://', 'https://', or '//'.");
@@ -29,7 +35,9 @@ enter url. make sure to start with http or:// https://.\n\n\n\
     };
   };
   if ("" != url && null != url && "https://a.mutualbooks.biz/go/https://null" != url) {
+    alert("DEBUG no cancel detected");
     if (url.includes("//", 0) || url.includes("http://", 0) || url.includes("https://", 0)) {
+      alert("DEBUG valid url")
       document.write("<title>UBer 3.5</title><div position:absolute;left:0;right:0;bottom:0;top:0px><iframe src='" + url + "' id='uberFrame' style='position:fixed;top:0px;bottom:0px;right:0px;width:100%;border:none;margin:0;padding:0;overflow:hidden;z-index:999999;height:100%;'>></iframe></div>");
       window.onbeforeunload = function() {
         /* lyft implementation built-in Just in Case(tm) :) */
